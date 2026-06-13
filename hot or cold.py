783435dedff52ebcf1570167ed779d9a76f691e8
ForLoops <angshumaan19@gmail.com>
2026-06-13 14:54:53 +0530

    hot or cold?

---
+++ hot or cold.py
@@ -0,0 +1,7 @@
+s_or_w = input("hot or cold?")
+if s_or_w == "hot":
+    print("t-shirt,pant,jeans,cap")
+elif s_or_w == "cold":
+    print("jacket,socks,mufflers,beanie")
+else:
+    print("please give a proper input")
