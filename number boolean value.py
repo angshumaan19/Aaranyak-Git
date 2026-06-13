d06020a8a6a469b1cb3b35cf0bfa385dc5bf54ba
ForLoops <angshumaan19@gmail.com>
2026-06-13 13:30:29 +0530

    boolean

---
+++ number boolean value.py
@@ -0,0 +1,22 @@
+a=6
+x=9
+s=0
+
+if a and x and s:
+    print("all three boolean values are true")
+else:
+    print("one of the values are false")
+
+n=0
+f=5
+b=-8
+
+if f > 0 or b > 0:
+    print("one of the values is greater than 0")
+else:
+    print("all are less than 0")
+
+if f > 0 or n> 0:
+    print("one of the values are greater than 0")
+else:
+    print("all are less than 0")
