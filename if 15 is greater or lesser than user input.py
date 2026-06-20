00b49680b1b1ce57fd001515cf92a92ab7bf0325
ForLoops <angshumaan19@gmail.com>
2026-06-20 19:43:07 +0530

    < 15 or > 15

---
+++ if 15 is greater or lesser than user input.py
@@ -0,0 +1,9 @@
+f=int(input("enter a integer"))
+
+if f < 15:
+    print("the entered value is < 15,i am in if block")
+elif f > 15:
+    print("the entered value is > 15,i am in elif block")
+else:
+    print("the entered value is = 15,i am in else block")
+
