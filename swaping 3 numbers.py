e66fbb079f4bb3433575e6035fa059ae6ef6d74c
ForLoops <angshumaan19@gmail.com>
2026-06-21 14:57:08 +0530

    this project aims to swap numbers in a list

---
+++ swaping 3 numbers.py
@@ -0,0 +1,12 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+
+k=[a,b,c]
+print(k)
+print("im going to swap the numbers")
+def my_function(lst):
+    k.reverse()
+    return lst
+print(my_function(k))
+
