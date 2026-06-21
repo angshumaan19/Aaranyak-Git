9e9e1ef7bb3748de239118095601cdc7b433f2e3
ForLoops <angshumaan19@gmail.com>
2026-06-21 13:30:32 +0530

    precedence of different operators

---
+++ operator precedence.py
@@ -0,0 +1,15 @@
+a=7
+b=4
+c=2
+d=5
+e=0
+e=(a+b)*c/d
+print("(a+b)*c/d =",e)
+
+name = "Josh"
+age = 3
+
+if name == "josh" or name == "gola" and age >= 4:
+    print("hello welcome!")
+else:
+    print("goodbye!!")
