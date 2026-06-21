3f40ccb02e20e5b3a8538e55ad9208a208d59140
ForLoops <angshumaan19@gmail.com>
2026-06-21 13:47:27 +0530

    divisible numbers with numerator and denominator

---
+++ divisible numbers.py
@@ -0,0 +1,13 @@
+print("enter the numerator")
+x=int(input())
+print("enter the denominator")
+f=int(input())
+
+if x%f==0:
+    print("\n "+str(x)+" is divisible by "+str(f))
+else:
+    print("\n "+str(x)+" is not divisible by "+str(f))
+
+
+
+
