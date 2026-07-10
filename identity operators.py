aa30a8b2757dcb9b917406c0988ea6d6c6716497
ForLoops <angshumaan19@gmail.com>
2026-07-10 19:18:01 +0530

    identity operator

--- identity operators.py
+++ identity operators.py
@@ -1,12 +1,12 @@
 s=3
-if (type(s) is int):
+if type(s) is int:
     print("true")
 else:
     print("false")
 
 c=6.3
 
-if (type(c) is not float):
+if type(c) is not float:
     print("true")
 else:
     print("false")
@@ -14,10 +14,10 @@ else:
 t=20
 u=20
 
-if (t is u):
+if t is u:
     print("t & u have the same identity")
 
 u=30
 
-if (t is not u):
+if t is not u:
     print("t & u have different identity")
