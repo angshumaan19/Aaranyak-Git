74c463da8ab083254aa3c12f19267dbefadd5edf
ForLoops <angshumaan19@gmail.com>
2026-07-25 13:59:23 +0530

    a calculator using functions

---
+++ calculator.py
@@ -0,0 +1,20 @@
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+
+def addition(x,y):
+    return x + y
+
+def subtraction(x,y):
+    return x - y
+
+def multiplication(x,y):
+    return x*y
+
+def division(x,y):
+    return x // y
+
+
+print(addition(x,y))
+print(subtraction(x,y))
+print(multiplication(x,y))
+print(division(x,y))
