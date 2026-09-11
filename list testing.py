db4cf9aa43e190d772e2f6b74f6a17302e0a97bb
ForLoops <angshumaan19@gmail.com>
2026-09-11 19:53:14 +0530

    lists

---
+++ list testing.py
@@ -0,0 +1,14 @@
+list1 = [1,2,"coder","pycharm",[1.02,5,"codingal"]]
+print(list1)
+print(type(list1))
+#indexing
+list2=[1,2,3,"brain",6,7,8,9]
+print(list2[3])
+#slicing
+print(list2[4:7])
+#concatination
+print(list1+list2)
+print(list1*4)
+#iteration through a for loop
+for i in list2:
+    print(i)
