fc5917adf8cd874de14bf11bcb1159476d8dbe00
ForLoops <angshumaan19@gmail.com>
2026-09-12 13:27:21 +0530

    tuple coding

---
+++ tuples.py
@@ -0,0 +1,14 @@
+tuplex = (1,2,"python",4,5,"coding",7)
+print(tuplex)
+
+tuplex2 = ("animal",5,7,9,1)
+print(tuplex2)
+
+tuplex3 = tuplex2 + (9,4,5)
+print(tuplex3)
+
+print(len(tuplex))
+
+print(tuplex2[3])
+
+print(tuplex[2:-1])
