56f5f90f4bf6b567cd9e7c91eedc6fbd5c41bbe5
ForLoops <angshumaan19@gmail.com>
2026-09-18 19:55:33 +0530

    finding the country code

---
+++ finding country.py
@@ -0,0 +1,8 @@
+country_code = {'india' : '0079',
+                'australia' : '0056',
+                'nepal' : '0021'}
+print("country code for india-")
+print(country_code.get('india','not found'))
+print("country code for japan-")
+print(country_code.get('japan','not found'))
+
